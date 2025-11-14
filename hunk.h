7cafad04f2a6bba8473bec7d663b4b3b444c4589
sebastianwhiffen <sebastianwhiffen@gmail.com>
2025-11-15 01:11:48 +1100

    update blurb

--- hunk.h
+++ hunk.h
@@ -4,7 +4,7 @@
 
 void H_MemInfo();
 
-void H_InitHunk();
+int H_InitHunk();
 
 #define QMEM_IMPLEMENTATION
 #ifdef QMEM_IMPLEMENTATION
@@ -31,36 +31,50 @@ void H_MemInfo() { printf("hi"); }
 //	internal representation after (uint8_t *)
 //
 //	intptr_t (16 byte / 64 bit) representation of the pointer:
-// 	64  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 0
-//      48  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 0
-//      32  0 0 0 0  0 0 0 0   1 0 1 0  0 0 0 0
-//      16  0 0 0 0  0 0 0 0   0 0 0 1  1 1 1 1
-//
-//	add another 31 so that it doesnt mask under total
+//	5721030666
 //	64  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 0
-//	48  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 0
-//	32  0 0 0 0  0 0 0 0   1 0 1 0  0 0 0 0
-//	16  0 0 0 0  0 0 0 0   0 0 1 1  1 1 1 0
+//	48  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 1
+//	32  0 1 0 1  0 1 0 1   0 0 0 0  0 0 0 0
+//	16  0 0 0 0  0 0 0 0   0 0 0 0  1 0 1 0
 //
-//	& ~31
+//	add another 31. when we align; it will not be below the hunkTotal
+//	(if it needs to be rounded down)
+//
+//	5721030697
+//	64  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 0
+//	48  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 1
+//	32  0 1 0 1  0 1 0 1   0 0 0 0  0 0 0 0
+//	16  0 0 0 0  0 0 0 0   0 0 1 0  1 0 0 1
+//
+//	& ~31 (round to nearest 32)
 //      64  1 1 1 1  1 1 1 1   1 1 1 1  1 1 1 1
 //	48  1 1 1 1  1 1 1 1   1 1 1 1  1 1 1 1
 //	32  1 1 1 1  1 1 1 1   1 1 1 1  1 1 1 1
 //	16  1 1 1 1  1 1 1 1   1 1 1 0  0 0 0 0
 //
-//	= 10485792
+//	= 5721030688
 //	64  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 0
-//	48  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 0
-//	32  0 0 0 0  0 0 0 0   1 0 1 0  0 0 0 0
+//	48  0 0 0 0  0 0 0 0   0 0 0 0  0 0 0 1
+//	32  0 1 0 1  0 1 0 1   0 0 0 0  0 0 0 0
 //	16  0 0 0 0  0 0 0 0   0 0 1 0  0 0 0 0
+//				     |
+//	thanks to the low bits found in ~31, the last 5 positions are dropped
 //
-//	essentially, the original size of the hunk was: 10485760 bytes
-//	after padding:					10485792 bytes
+//	essentially, we started with 5721030666 bytes
+//	(which is not divisible by 32)
+//	and we ended with a padded region of 5721030688
+//	(which is)
 //
-//	this promisies that the returned bytes are
+//	we lose the tinyest bit of memory at begining of the pointer by
+// 	increasing its raw value
+// 	(hunkData is now pointing to a memory address 32-ish bytes
+// 	infront of what was actually reserved)
+//
+//	but we gain perf improvements due to the cpu pulling in even chunks
+//	into the cache
 // ------------------------------------------------------------------------------
 
-void H_InitHunk(int minMegsAlloc) {
+int H_InitHunk(int minMegsAlloc) {
   hunkTotal = minMegsAlloc * 1024 * 1024;
   int cachelineMask = 31;
   printf("intended hunk total: %d \n", hunkTotal);
@@ -68,13 +82,18 @@ void H_InitHunk(int minMegsAlloc) {
   hunkData = (uint8_t *)calloc(hunkTotal + cachelineMask, 1);
 
   if (!hunkData) {
-    printf("hunk failed to init with: %d bytes \n", (hunkTotal + cachelineMask));
+    printf("hunk failed to init with: %d bytes \n",
+           (hunkTotal + cachelineMask));
+    return 0;
   }
 
   printf("hunk initialised with: %d bytes \n", (hunkTotal + cachelineMask + 1));
 
+  // this will always align it to the mask
   hunkData =
       (uint8_t *)(((uintptr_t)hunkData + cachelineMask) & ~cachelineMask);
+
+  return 1;
 }
 
 #endif
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #define QMEM_IMPLEMENTATION
 #include "hunk.h"
+#include<unistd.h>
 
 int main() {
-  H_InitHunk(10);
+  H_InitHunk(1000);
+  sleep(9999999);
   // H_MemInfo();
 }
